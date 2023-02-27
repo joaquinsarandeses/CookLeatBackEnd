dabdb9a731695e5fcf13ef71eec95f52090aeb2f
joaquinsarandeses <joaquinsarandeses@gmail.com>
2023-02-27 21:15:18 +0100

    errorsUpdate
    Correción de errores y mejora en la creacción de usuarios

--- CookLeat/database/migrations/2014_08_19_000000_create_users_table.php
+++ CookLeat/database/migrations/2014_08_19_000000_create_users_table.php
@@ -15,11 +15,13 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name', 50);
-            $table->string('email');
-            $table->string('password', 60);
-            $table->string('image');
-            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('image')->nullable()->default(null);
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
         });
     }
 
